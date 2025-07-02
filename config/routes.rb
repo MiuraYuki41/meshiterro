56954bba827936cd143450f80cf5ec867a766d00
MiuraYuki41 <205943986+MiuraYuki41@users.noreply.github.com>
2025-07-02 23:21:07 +0000

    commit3

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  resources :post_images, only: [:new, :create, :index, :show]
+  get 'users/show'
+  get 'users/edit'
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resource :favorite, only: [:create, :destroy]
+    resources :post_comments, only: [:create, :destroy]
+  end
+  resources :users, only: [:show, :edit, :update]
   devise_for :users
   root to: "homes#top"
   get '/homes/about' => 'homes#about'
